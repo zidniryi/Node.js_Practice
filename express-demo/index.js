2621fb439a35c7bd42bc0b73cd424fe29a0e11ba
zidniryi <zidnirydwan@gmail.com>
2019-08-25 20:00:26 +0700

    basic express get data

--- express-demo/index.js
+++ express-demo/index.js
@@ -2,8 +2,12 @@ var express = require('express')
 const app = express()
 
 // Define Routes and callback
-app.get('/', (res, req) =>{
+app.get('/', (req, res) => {
     res.send('Hello World')
+})
+
+app.get('/api/courses', (req, res) =>{
+    res.send([1,2,3])
 })
 
 app.listen(4000, () => console.log('Listening on port 4000 . . . .'))
