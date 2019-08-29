818d4ed3e2d4312b733ef9bbc6eae5e8ffab4f44
zidniryi <zidnirydwan@gmail.com>
2019-08-29 22:18:30 +0700

    callback

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,2 +1,24 @@
 console.log('Before')
+getUser(1,  (user)=>{
+    getRepo(user.gitUser, (repos) =>{
+        console.log('Repos', repos)
+    })
+})
+
 console.log('After')
+
+function getUser(id, callback) {
+    setTimeout(() => {
+        console.log('Reading data from db ...')
+        callback({id: id, gitUser: 'zidniryi'})
+        return
+    }, 2000)
+
+}
+
+function getRepo(username, callback) {
+    setTimeout(() => {
+        callback(['repo1', 'repo2', 'repo3'])
+
+    }, 2000)
+}
