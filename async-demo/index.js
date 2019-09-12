260723590851ca4c58de254d400892beacda75db
zidniryi <zidnirydwan@gmail.com>
2019-09-12 23:20:32 +0700

    embeding data mongo fix and clean

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,30 +1,63 @@
 console.log('Before');
-getUser(1, (user) => {
-  getRepositories(user.gitHubUsername, (repos) => {
-    getCommits(repos[0], (commits) => {
-      console.log(commits);
-    })
-  })
-});
+
+// Callback-based approach
+//
+// getUser(1, (user) => {
+//   getRepositories(user.gitHubUsername, (repos) => {
+//     getCommits(repos[0], (commits) => {
+//       console.log(commits);
+//     })
+//   })
+// });
+
+// Promise-based approach
+// getUser(1)
+//   .then(user => getRepositories(user.gitHubUsername))
+//   .then(repos => getCommits(repos[0]))
+//   .then(commits => console.log('Commits', commits))
+//   .catch(err => console.log('Error', err.message));
+
+async function getDisplay(){
+
+
+    try {
+        const user = await getUser(1)
+        const repos = await getRepositories(user.gitHubUsername)
+        const commits = await getCommits(repos[0])
+        console.log(commits)
+    } catch (error) {
+        console.log('Error', error.message)
+    }
+}
+
+getDisplay()
 console.log('After');
 
-function getUser(id, callback) {
-  setTimeout(() => {
-    console.log('Reading a user from a database...');
-    callback({ id: id, gitHubUsername: 'mosh' });
-  }, 2000);
+function getUser(id) {
+  return new Promise((resolve, reject) => {
+    // Kick off some async work 
+    setTimeout(() => {
+      console.log('Reading a user from a database...');
+      resolve({ id: id, gitHubUsername: 'mosh' });
+    }, 2000);
+  });
 }
 
-function getRepositories(username, callback) {
-  setTimeout(() => {
-    console.log('Calling GitHub API...');
-    callback(['repo1', 'repo2', 'repo3']);
-  }, 2000);
+function getRepositories(username) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Calling GitHub API...');
+      resolve(['repo1', 'repo2', 'repo3']);
+    // reject(new Error('message'))
+    }, 2000);  
+  });
 }
 
-function getCommits(repo, callback) {
-  setTimeout(() => {
-    console.log('Calling GitHub API...');
-    callback(['commit']);
-  }, 2000);
+function getCommits(repo) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('Calling GitHub API...');
+      resolve(['commit']);
+    }, 2000);
+  });
 }
