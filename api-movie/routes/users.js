43c07ad59e73e1893b503ab34d604e91b2a7c133
zidniryi <zidnirydwan@gmail.com>
2019-09-22 22:07:07 +0700

    add register, hashing password and auth

--- api-movie/routes/users.js
+++ api-movie/routes/users.js
@@ -1,5 +1,7 @@
+const bcrypt = require('bcrypt')
 const {User, validate} = require('../models/user')
 const express = require('express');
+const _ = require('lodash')
 const router = express.Router();
 
 
@@ -12,13 +14,13 @@ router.post('/', async (req, res) => {
     if(user) return res.status(400).send('User already register')
 
     // Post else
-    user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    })
+    user = new User(_.pick(req.body, ['name','email', 'password']))
+    // For hash
+    const salt = await bcrypt.genSalt(10)
+    user.password = await bcrypt.hash(user.password, salt)
      await user.save()
-     res.send(user)
+    //  Using lodash
+     res.send(_.pick(user, ['_id','name', 'email']))
 });
 
 
