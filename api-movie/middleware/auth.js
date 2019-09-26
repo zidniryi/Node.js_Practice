a10eb7cae56e851c996f610308f16a97b6798813
zidniryi <zidnirydwan@gmail.com>
2019-09-26 22:38:18 +0700

    iddleware

---
+++ api-movie/middleware/auth.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken')
+const config = require('config')
+module.exports =  function (req, res, next) {
+  // Middelware to verfiy jwt token on the header
+  const token = res.header('x-auth-token')
+  if (!token) return res.status(401).send('Acces denied, no token provided')
+
+    try {
+        const decoded = jwt.verify(token, config.get('jwtPrivateKey')) 
+        req.user = decoded
+        next()
+    } catch (error) {
+        res.status(400).send('Invalid token.')      
+    }
+}
