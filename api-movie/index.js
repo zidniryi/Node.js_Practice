929d64faaa1b9e7a2f72f94ef69bc7d75c89b12e
zidniryi <zidnirydwan@gmail.com>
2019-11-03 13:56:09 +0700

    refactor config for better clean code

--- api-movie/index.js
+++ api-movie/index.js
@@ -1,4 +1,3 @@
-const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const express = require('express');
@@ -6,6 +5,7 @@ const app = express();
 require('./startup/logging')
 require('./startup/routes')(app)
 require('./startup/db')()
+require('./startup/config')()
 
 // Hanlde uncaught
 // process.on('uncaughtException', (ex) => {
@@ -16,10 +16,7 @@ require('./startup/db')()
 // const p = Promise.reject(new Error('Error Misereble hi'))
 // p.then(() => console.log('Done'))
 
-if (!config.get('jwtPrivateKey')) {
-  console.error('FATAL ERROR: jwtPrivateKey is not defined.');
-  process.exit(1);
-}
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
