46676051fac845435444b1b4c6cfed7b614a5e4e
zidniryi <zidnirydwan@gmail.com>
2019-11-03 13:17:18 +0700

    refactor index router onto startup

--- api-movie/index.js
+++ api-movie/index.js
@@ -14,24 +14,31 @@ const users = require('./routes/users');
 const auth = require('./routes/auth');
 const express = require('express');
 const app = express();
+require('./startup/routes')(app)
 // Hanlde uncaught
-process.on('uncaughtException', (ex) => {
-  winston.error(ex.message, ex)
-  process.exit(1)
-})
+// process.on('uncaughtException', (ex) => {
+//   winston.error(ex.message, ex)
+//   process.exit(1)
+// })
+
+// Handle with winston
+winston.handleExceptions(
+  new winston.transports.File({ filename: 'exceptionError.log' })
+)
 
 // Handle promise reject
 process.on('unhandledRejection', (ex) => {
-  winston.error(ex.message, ex)
-  process.exit(1)
+  // winston.error(ex.message, ex)
+  // process.exit(1)
+  throw (ex)
 })
 
 // winston.add(winston.transports.File, { filename: 'logfile.log' })
 winston.add(winston.transports.File, { filename: 'logging.log' })
 winston.add(winston.transports.MongoDB, { db: 'mongodb://localhost/vidly' })
 
-const p = Promise.reject(new Error('Error Misereble hi'))
-p.then(() => console.log('Done'))
+// const p = Promise.reject(new Error('Error Misereble hi'))
+// p.then(() => console.log('Done'))
 
 if (!config.get('jwtPrivateKey')) {
   console.error('FATAL ERROR: jwtPrivateKey is not defined.');
@@ -42,15 +49,7 @@ mongoose.connect('mongodb://localhost/vidly')
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err, { level: 'info' }));
 
-app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-// Express error middleware
-app.use(error)
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
---
+++ api-movie/startup/routes.js
@@ -0,0 +1,21 @@
+// Separate import
+const express = require('express')
+const error = require('../middleware/error')
+const genres = require('../routes/genres');
+const customers = require('../routes/customers');
+const movies = require('../routes/movies');
+const rentals = require('../routes/rentals');
+const users = require('../routes/users');
+const auth = require('../routes/auth');
+// export app as argument
+module.exports = function (app) {
+    app.use(express.json());
+    app.use('/api/genres', genres);
+    app.use('/api/customers', customers);
+    app.use('/api/movies', movies);
+    app.use('/api/rentals', rentals);
+    app.use('/api/users', users);
+    app.use('/api/auth', auth);
+    // Express error middleware
+    app.use(error)
+}
