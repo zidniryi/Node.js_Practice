636a882c13016fd6554ff28ff535c40b88da8eb3
zidniryi <zidnirydwan@gmail.com>
2019-11-03 22:44:58 +0700

    refactoring code with confidence

--- jest-testing/lib.js
+++ jest-testing/lib.js
@@ -3,9 +3,8 @@ const mail = require('./mail');
 
 // Testing numbers 
 module.exports.absolute = function (number) {
-  if (number > 0) return number;
-  if (number < 0) return -number;
-  return 0;
+  if (number >= 0) return number;
+  return -number;
 }
 
 // // Testing strings 
--- jest-testing/tests/lib.test.js
+++ jest-testing/tests/lib.test.js
@@ -1,14 +1,17 @@
 const lib = require('../lib')
-test('Absolute - should return + number if input is +', () => {
-    const result = lib.absolute(1)
-    expect(result).toBe(1)
-})
+describe('absolute', () => {
+    it('should return + number if input is +', () => {
+        const result = lib.absolute(1)
+        expect(result).toBe(1)
+    })
 
-test('Absolute - should return + number if input is -', () => {
-    const result = lib.absolute(-1)
-    expect(result).toBe(1)
-})
-test('Absolute - should return 0 number if input is 0', () => {
-    const result = lib.absolute(0)
-    expect(result).toBe(0)
-})
+    it('should return + number if input is -', () => {
+        const result = lib.absolute(-1)
+        expect(result).toBe(1)
+    })
+    it('should return 0 number if input is 0', () => {
+        const result = lib.absolute(0)
+        expect(result).toBe(0)
+    })
+});
+
